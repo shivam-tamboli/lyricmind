0cb9f51a53bf444e96b3b2404c2c01e68770840f
Shivam <shivamtamboli66@gmail.com>
2025-12-23 16:38:12 +0530

    first commit

---
+++ src/test/java/com/lyricmind/LyricmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyricmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
