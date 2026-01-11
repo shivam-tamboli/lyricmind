fd4685fab982a74e3b203df9951746cc671ffc64
Shivam <shivamtamboli66@gmail.com>
2026-01-11 20:09:24 +0530

    add SongRecommendationResponse DTO

--- src/main/java/com/lyricmind/model/SongRecommendation.java
+++ src/main/java/com/lyricmind/model/SongRecommendation.java
@@ -1,4 +1,27 @@
 package com.lyricmind.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Map;
+import java.util.Objects;
+
+@Getter
+@Setter
 public class SongRecommendation {
+
+    private Song song;
+    private Map<String, Objects> metadata;
+    private double similarityScore;
+    private String reasonForRecommendation;
+
+    public SongRecommendation(Song song, Map<String, Objects> metadata, double similarityScore, String reasonForRecommendation) {
+        this.song = song;
+        this.metadata = metadata;
+        this.similarityScore = similarityScore;
+        this.reasonForRecommendation = reasonForRecommendation;
+    }
+
+    private String generateReasonForRecommendation(){
+        return metadata.get("motivation").toString();
+    }
 }
