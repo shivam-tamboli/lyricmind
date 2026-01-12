74d696436a4e2d649bf78513750089a36fdfb9f8
Shivam <shivamtamboli66@gmail.com>
2026-01-12 17:43:55 +0530

    add SongRepository interface

--- src/main/java/com/lyricmind/repository/SongRepository.java
+++ src/main/java/com/lyricmind/repository/SongRepository.java
@@ -1,4 +1,9 @@
 package com.lyricmind.repository;
 
-public interface SongRepository {
+import com.lyricmind.model.Song;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SongRepository extends MongoRepository<Song, String> {
 }
