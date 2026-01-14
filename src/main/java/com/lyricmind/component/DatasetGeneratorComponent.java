265f74c9d7c42b58ee442eabec9053d9f66bf506
Shivam <shivamtamboli66@gmail.com>
2026-01-14 23:11:48 +0530

    add objectmapper

--- src/main/java/com/lyricmind/component/DatasetGeneratorComponent.java
+++ src/main/java/com/lyricmind/component/DatasetGeneratorComponent.java
@@ -1,7 +1,10 @@
 package com.lyricmind.component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
 @Component //This make it a spring component.
 public class DatasetGeneratorComponent {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 }
