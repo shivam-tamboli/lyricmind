4149344aeeb605eedcd557a30f3b52cb08aa080a
Shivam <shivamtamboli66@gmail.com>
2026-01-15 23:55:58 +0530

    update

--- src/main/java/com/lyricmind/component/DatasetGeneratorComponent.java
+++ src/main/java/com/lyricmind/component/DatasetGeneratorComponent.java
@@ -1,10 +1,14 @@
 package com.lyricmind.component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component //This make it a spring component.
 public class DatasetGeneratorComponent {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private Logger logger = LoggerFactory.getLogger(DatasetGeneratorComponent.class);
+
 }
