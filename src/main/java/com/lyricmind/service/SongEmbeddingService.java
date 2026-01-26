bc13f32d507dae6cb80e0b0aa830abd64e5f3442
Shivam <shivamtamboli66@gmail.com>
2026-01-27 03:06:03 +0530

    Constructor based dependency injection in SongEmbeddingService

--- src/main/java/com/lyricmind/service/SongEmbeddingService.java
+++ src/main/java/com/lyricmind/service/SongEmbeddingService.java
@@ -1,7 +1,21 @@
 package com.lyricmind.service;
 
+import com.lyricmind.component.DatasetGeneratorComponent;
+import com.lyricmind.repository.SongRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class SongEmbeddingService {
+
+    private static final String RESOURCES_PATH = "src/main/resources/";
+
+    private final SongRepository songRepository;
+    private final VectorStore vectorStore;
+    private final DatasetGeneratorComponent datasetGeneratorComponent;
+
 }
