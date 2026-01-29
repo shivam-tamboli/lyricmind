c3c07d970472c0780e5a63aed3b34e487d502a01
Shivam <shivamtamboli66@gmail.com>
2026-01-29 21:21:51 +0530

    complete RecommendationController

--- src/main/java/com/lyricmind/controller/RecommendationController.java
+++ src/main/java/com/lyricmind/controller/RecommendationController.java
@@ -1,14 +1,38 @@
 package com.lyricmind.controller;
 
+import com.lyricmind.model.dto.MusicRequest;
+import com.lyricmind.model.dto.SongRecommendationResponse;
+import org.slf4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.lyricmind.service.RecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/lyricmind/v1/recommendations")
 public class RecommendationController {
 
+
     @Autowired
     RecommendationService recommendationService;
+    Logger logger = LoggerFactory.getLogger(RecommendationController.class);
+
+    @PostMapping
+    public ResponseEntity<List<SongRecommendationResponse>> recommendSongs(
+            @RequestBody MusicRequest request) {
+
+        List<SongRecommendationResponse> recommendations = recommendationService.recommendSongs(
+                request.mood(),
+                request.limit() != null ? request.limit() : 10
+        );
+        return ResponseEntity.ok(recommendations);
+    }
 }
