c840787393110c5fd18d9988d3349e84437edee6
Shivam <shivamtamboli66@gmail.com>
2026-01-29 21:09:45 +0530

    RecommendationController annotates @RequestMapping and @Autowired RecommendationService in RecommendationController

--- src/main/java/com/lyricmind/controller/RecommendationController.java
+++ src/main/java/com/lyricmind/controller/RecommendationController.java
@@ -1,7 +1,14 @@
 package com.lyricmind.controller;
 
+import com.lyricmind.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/lyricmind/v1/recommendations")
 public class RecommendationController {
+
+    @Autowired
+    RecommendationService recommendationService;
 }
