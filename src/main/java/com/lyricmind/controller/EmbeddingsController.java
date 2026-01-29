aef0f7be95d2ea8bcf123f0f0cc954dce701c12a
Shivam <shivamtamboli66@gmail.com>
2026-01-29 17:45:54 +0530

    add complete EmbeddingsController for bulk song

--- src/main/java/com/lyricmind/controller/EmbeddingsController.java
+++ src/main/java/com/lyricmind/controller/EmbeddingsController.java
@@ -1,7 +1,12 @@
 package com.lyricmind.controller;
 
+import com.lyricmind.model.dto.BulkSongRequest;
+import com.lyricmind.model.dto.BulkSongResponse;
 import com.lyricmind.service.SongEmbeddingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,8 @@ public class EmbeddingsController {
 
     @Autowired
     SongEmbeddingService songEmbeddingService;
+
+    ResponseEntity<BulkSongResponse> createEmbeddingFromBulkSong(@RequestBody BulkSongRequest request){
+        return new ResponseEntity<>(songEmbeddingService.createEmbeddingFromBulkSong(request), HttpStatus.CREATED);
+    }
 }
