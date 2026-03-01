f61aa95911dfeb5949783b8cb867421a2128e494
Shivam <shivamtamboli66@gmail.com>
2026-03-01 19:09:00 +0530

    add metadata

--- src/test/java/com/lyricmind/service/RecommendationServiceTest.java
+++ src/test/java/com/lyricmind/service/RecommendationServiceTest.java
@@ -5,12 +5,15 @@ import com.lyricmind.component.SemanticQueryComponent;
 import com.lyricmind.model.Song;
 import com.lyricmind.repository.SongRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.swing.text.Document;
+import org.springframework.ai.document.Document;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @ExtendWith(MockitoExtension.class)                                    //Enable mock injection automatically
@@ -28,17 +31,24 @@ public class RecommendationServiceTest {
     @InjectMocks
     private RecommendationService recommendationService;
 
-    private Song testSong;
+    private Song testSong;                                              //shared test objects
     private Document testDocument;
 
-    @BeforeEach
+    @BeforeEach                                                        //this method runs before every test cases.
     void setUp() {
-        testSong = new Song();
+        testSong = new Song();                                          //dummy test data.
         testSong.setId("song123");
         testSong.setTitle("Test Song");
         testSong.setArtist("Test Artist");
         testSong.setAlbum("Test Album");
         testSong.setGenre("Rock");
         testSong.setReleaseYear(2023);
+
+        Map<String, Object> metadate = new HashMap<>();                 //sample document.
+        metadate.put("songId", "song123");
+        metadate.put("motivation", "Perfect match for your mood");
+
+        testDocument = new Document("Test Content", metadate);
     }
+
 }
