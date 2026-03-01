7089726ba3e04cbcf55ddce605ce6de9ce264965
Shivam <shivamtamboli66@gmail.com>
2026-03-01 18:57:19 +0530

    create obj add setUp method with fake info

--- src/test/java/com/lyricmind/service/RecommendationServiceTest.java
+++ src/test/java/com/lyricmind/service/RecommendationServiceTest.java
@@ -2,11 +2,15 @@ package com.lyricmind.service;
 
 import com.lyricmind.component.RerankComponent;
 import com.lyricmind.component.SemanticQueryComponent;
+import com.lyricmind.model.Song;
 import com.lyricmind.repository.SongRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.swing.text.Document;
 
 
 @ExtendWith(MockitoExtension.class)                                    //Enable mock injection automatically
@@ -23,4 +27,18 @@ public class RecommendationServiceTest {
 
     @InjectMocks
     private RecommendationService recommendationService;
+
+    private Song testSong;
+    private Document testDocument;
+
+    @BeforeEach
+    void setUp() {
+        testSong = new Song();
+        testSong.setId("song123");
+        testSong.setTitle("Test Song");
+        testSong.setArtist("Test Artist");
+        testSong.setAlbum("Test Album");
+        testSong.setGenre("Rock");
+        testSong.setReleaseYear(2023);
+    }
 }
