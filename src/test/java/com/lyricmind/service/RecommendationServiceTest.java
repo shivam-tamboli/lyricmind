254d75b8784d9c02deca6f2491360919a4a95f5d
Shivam <shivamtamboli66@gmail.com>
2026-03-01 18:46:04 +0530

    use @Mock anno to create Fake ojects

--- src/test/java/com/lyricmind/service/RecommendationServiceTest.java
+++ src/test/java/com/lyricmind/service/RecommendationServiceTest.java
@@ -1,4 +1,22 @@
 package com.lyricmind.service;
 
+import com.lyricmind.component.RerankComponent;
+import com.lyricmind.component.SemanticQueryComponent;
+import com.lyricmind.repository.SongRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)                                    //Enable mock injection automatically
 public class RecommendationServiceTest {
+
+    @Mock
+    private SongRepository songRepository;
+
+    @Mock
+    private RerankComponent rerankComponent;
+
+    @Mock
+    private SemanticQueryComponent semanticQueryComponent;
 }
